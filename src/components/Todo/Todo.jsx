ca1e3fbded80875c4497693c7eec3fe4c90a15b5
kvvivvic <rnjs9337@gmail.com>
2023-01-11 17:40:08 +0900

    list  스타일 추가

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
+import styles from "./Todo.module.css";
 
 const Todo = ({ todo, onUpdate, onDelete }) => {
   const { text, status } = todo;
@@ -11,12 +12,16 @@ const Todo = ({ todo, onUpdate, onDelete }) => {
     onDelete(todo);
   };
   return (
-    <li>
-      <input type="checkbox" id="checkbox" checked={status === "completed"} onChange={handleChange} />
-      <label htmlFor="checkbox">{text}</label>
-      <button onClick={handleDelete}>
-        <FaTrashAlt />
-      </button>
+    <li className={styles.todo}>
+      <input className={styles.checkbox} type="checkbox" id="checkbox" checked={status === "completed"} onChange={handleChange} />
+      <label className={styles.text} htmlFor="checkbox">
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.btn}>
+          <FaTrashAlt />
+        </button>
+      </span>
     </li>
   );
 };
